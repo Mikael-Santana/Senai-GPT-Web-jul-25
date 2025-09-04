4441984afe5f81c54fa227c1d63dcc523c39d663
mikael <mikaelsantana249@gmail.com>
2025-09-03 21:59:27 -0300

    bained do campo de login

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { EmailValidator, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-screen.html',
   styleUrl: './login-screen.css'
 })
 export class LoginScreen {
+
+  loginForm:  FormGroup;
+
+  constructor (private fb: FormBuilder) {
+    // quando a tela iniciar.
+    
+    //inicia o formulario:
+    //cria o campo obrigatorio do email.
+    //cria o campo obrigadorio da senha.
+
+    this.loginForm = this.fb.group ({
+
+      email: ["",[Validators.required]],
+      passaword: ["",[Validators.required]]
+
+    });
+
+  }
+
+  onLoginClick() {
+
+    alert ("Botao de login clicado.");
+
+    console.log ("email", this.loginForm.value.email);
+    console.log ("passaword",  this.loginForm.value.passaword);
+
+  }
 
 }
