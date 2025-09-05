0ee3d0d7dc8fccd6765ab5561c8e62298290bb26
mikael <mikaelsantana249@gmail.com>
2025-09-04 21:55:17 -0300

    Validacao de login

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -9,31 +9,61 @@ import { EmailValidator, FormBuilder, FormGroup, ReactiveFormsModule, Validators
 })
 export class LoginScreen {
 
-  loginForm:  FormGroup;
+  loginForm: FormGroup;
 
-  constructor (private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) {
     // quando a tela iniciar.
-    
+
     //inicia o formulario:
     //cria o campo obrigatorio do email.
     //cria o campo obrigadorio da senha.
 
-    this.loginForm = this.fb.group ({
+    this.loginForm = this.fb.group({
 
-      email: ["",[Validators.required]],
-      passaword: ["",[Validators.required]]
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]]
 
     });
 
   }
 
-  onLoginClick() {
+  async onLoginClick() {
 
-    alert ("Botao de login clicado.");
+    alert("Botao de login clicado.");
 
-    console.log ("email", this.loginForm.value.email);
-    console.log ("passaword",  this.loginForm.value.passaword);
+    console.log("email", this.loginForm.value.email);
+    console.log("password", this.loginForm.value.password);
 
+    if (this.loginForm.value.email == "") {
+      alert ("preencha o email.");
+      return;
+    }
+    if (this.loginForm.value.password == "") {
+      alert ("preencha a senha.");
+      return;
+    }
+
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+      method: "POST", // ENVIAR.
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      })
+    })
+
+    console.log("STATUS CODE", response.status);
+
+    if (response.status >= 200 && response.status < 300) {
+      console.log("✅ Deu bom!");
+      alert("✅ Deu bom! Login realizado com sucesso.");
+    } else {
+      console.log("❌ Deu ruim! Status:", response.status);
+      alert("❌ Deu ruim! Erro ao fazer login. Código: " + response.status);
+    }
+    
   }
 
 }
