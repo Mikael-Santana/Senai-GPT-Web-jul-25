28747dd8f667c7f1e2998a656a98f1420590799d
mikael <mikaelsantana249@gmail.com>
2025-09-09 20:13:25 -0300

    autenticacao segura na tela de chat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginScreen } from './user-module/login-screen/login-screen';
 import { ChatScreen } from './user-module/chat-screen/chat-screen';
+import { authGuard } from './auth.guard';
+
 
 export const routes: Routes = [
     {
         path:"login",
         loadComponent: () => LoginScreen
     },
+
     {
         path:"chat",
-        loadComponent: () => ChatScreen
+        loadComponent: () => ChatScreen,
+        canActivate: [authGuard]
     }
 ];
